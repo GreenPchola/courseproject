162f225bea609a9802d51a2058c45dc68346cfc0
Vlad <vlad-korenev24@mail.ru>
2020-10-12 20:30:20 +0300

    second publication

--- server/app.js
+++ server/app.js
@@ -1,4 +1,11 @@
 const express = require("express")
-
-
+const config = require("config")
 const app = express()
+const PORT = process.env.PORT||config.server.PORT;
+
+app.get("/", (req,res)=>{
+    res.send("Hello world")
+})
+app.listen(PORT, ()=>{
+    console.log(` server has started${PORT}`)
+})
